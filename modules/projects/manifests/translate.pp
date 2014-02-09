3473e95eec1676e67e4cc8be29ad57ab6b9ad905
Donald Piret <donald@donaldpiret.com>
2014-02-10 02:15:44 +0800

    Add project

--- modules/projects/manifests/translate.pp
+++ modules/projects/manifests/translate.pp
@@ -1,8 +1,9 @@
 class projects::translate {
-roomorama::project { 'translate':
-  mysql         => true,
-  nginx         => true,
-  ruby          => '1.9.3',
-  source        => 'https://github.com/roomorama/translate.git'
-}
+  roomorama::project { 'translate':
+    mysql         => true,
+    nginx         => true,
+    ruby          => '2.0.0',
+    source        => 'https://github.com/roomorama/translate.git',
+    server_aliases => 'translate.roomorama.dev roomorama-translate.dev'
+  }
 }
